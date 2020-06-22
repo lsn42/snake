7a7daaff7b2633abb14d642f55c7073c02dbfd09
lsn42 <24622966+lsn42@users.noreply.github.com>
2020-06-22 23:39:24 +0800

    nothing

--- 1.asm
+++ 1.asm
@@ -2,3 +2,16 @@ data segment
 food dw 0
 body dw 400 dup(?)
 data ends
+
+get_input proc near
+    mov al, 0
+    mov ah, 1
+    int 16h ; check data
+    cmp ah, 1
+    je get_input_end
+
+    mov al, 0
+    mov ah, 0
+    int 16h
+get_input_end:
+    ret
